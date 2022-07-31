0f88d506a478dc77fc0ccb2c8f4190842d381b13
Marcos Moura <marcosmoura201@hotmail.com>
2022-07-31 10:13:31 -0300

    Implementando a listagem da tela de livros

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
-import{ FiPower} from 'react-icons/fi'
+import{ FiPower, FiEdit, FiTrash2} from 'react-icons/fi'
 import './styles.css'
 import logoImage from '../../assets/logo.svg'
 
@@ -16,6 +16,104 @@ export default function Book(){
                     <FiPower size={18} color="#251FC5" />
                 </button>
             </header>
+
+            <h1>Registered Books</h1>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Niguel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Niguel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Niguel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Niguel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Niguel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }

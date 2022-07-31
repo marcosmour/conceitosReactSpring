95f548f67d004473e23e9b69e7631e3be8cdf359
Marcos Moura <marcosmoura201@hotmail.com>
2022-07-31 18:40:41 -0300

    Ajustes na versão do react-router-dom

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -11,10 +11,11 @@ export default function Books(){
             <header>
                 <img src={logoImage} alt="Erudio" />
                 <span>Welcome, <strong>Marcos</strong>!</span>
-                <Link className="button" to="book/new">Add New Book</Link>
+                <Link className="button" to="/book/new">Add New Book</Link>
                 <button type="button">
                     <FiPower size={18} color="#251FC5" />
                 </button>
+               
             </header>
 
             <h1>Registered Books</h1>

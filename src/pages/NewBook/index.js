2d527dc4f16b1072366c726c95c784ac12933230
Marcos Moura <marcosmoura201@hotmail.com>
2022-08-03 16:12:17 -0300

    Integrando a tela de cadastros de livros a API

--- src/pages/NewBook/index.js
+++ src/pages/NewBook/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import { Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {useNavigate, useParams, Link} from 'react-router-dom';
 import{ FiArrowLeft} from 'react-icons/fi'
+
+import api from "../../services/api";
+
 import './styles.css';
+
 import logoImage from '../../assets/logo.svg';
 
 export default function NewBook(){
+    const [id, setId] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState('');
+
+    const username = localStorage.getItem('username');
+    const accessToken=localStorage.getItem('accessToken');
+
+    const navigate = useNavigate();
+
+    async function createNewBook(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            author,
+            launchDate,
+            price,
+        }
+
+        const authorization = {
+            Authorization: `Bearer ${accessToken}`
+        }
+
+        try {
+             await api.post('api/book/v1', data, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            navigate('/books');
+        } catch (err) {
+            alert('Error while recording Book! Try again')
+        }
+}
     return(
         <div className="new-book-container">
             <div className="content">
@@ -17,15 +57,31 @@ export default function NewBook(){
                         Home
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Title" />
-                    <input placeholder="Author" />
-                    <input type="date" />
-                    <input placeholder="Price" />
+                <form onSubmit={createNewBook}>
+                    <input
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                         />
+                    <input
+                        placeholder="Author"
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}
+                         />
+                    <input
+                        type="date"
+                        value={launchDate}
+                        onChange={e => setLaunchDate(e.target.value)}
+                         />
+                    <input
+                        placeholder="Price"
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                         />
 
                     <button className="button" type="submit">Add</button>
                 </form>
             </div>
         </div>
     );
-}
+    }
